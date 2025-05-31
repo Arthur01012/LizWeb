80dafc7f084c2b0c17cfc9ad8d3ae5a813a54166
Arthur Reyes <142542790+Arthur01012@users.noreply.github.com>
2025-05-30 18:10:43 -0600

    fix playsong

--- scripts/songs.js
+++ scripts/songs.js
@@ -106,26 +106,3 @@ carouselSlide.addEventListener("mouseenter", () => {
 carouselSlide.addEventListener("mouseleave", () => {
     startInterval();
 });
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const songs = [
-        './assets/SongsAudios/AURORA - Exist For Love.mp3',
-        './assets/SongsAudios/BWU - Hazel Eyes.mp3',
-        './assets/SongsAudios/Damiano David - Angel.mp3'
-    ];
-
-    const player = document.getElementById('audioPlayer');
-
-    function playRandomSong() {
-        const randomIndex = Math.floor(Math.random() * songs.length);
-        player.src = songs[randomIndex];
-        player.play();
-    }
-
-    // Reproduce una al cargar
-    playRandomSong();
-
-    // Cuando termina, reproduce otra
-    player.addEventListener('ended', playRandomSong);
-});
